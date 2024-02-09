9d4e736d75bc6bb72860c4e2a4aa5a6a2e5a3c1b
sorheil <sorheitchokothe@gmail.com>
2024-02-09 15:35:01 +0100

    adding  new features

--- social_network/Database.sql
+++ social_network/Database.sql
@@ -18,9 +18,9 @@ CREATE TABLE
     IF NOT EXISTS community_roles_permissions (
         role_id bigint unsigned,
         permission_id bigint unsigned,
+        PRIMARY KEY (role_id, permission_id),
         FOREIGN KEY (role_id) REFERENCES community_roles (id),
-        FOREIGN KEY (permission_id) REFERENCES community_permissions (id),
-        PRIMARY KEY (role_id, permission_id)
+        FOREIGN KEY (permission_id) REFERENCES community_permissions (id)
     );
 
 CREATE TABLE
@@ -83,4 +83,99 @@ CREATE TABLE
         FOREIGN KEY (owner_id) REFERENCES profiles (id),
         FOREIGN KEY (member_default_role_id) REFERENCES community_roles_permissions (role_id),
         FOREIGN KEY (visitor_role_id) REFERENCES community_roles_permissions (role_id)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS communities_members (
+        id bigint unsigned PRIMARY KEY AUTO_INCREMENT,
+        profile_id bigint unsigned,
+        community_id bigint unsigned,
+        role_id bigint unsigned,
+        joined_at TIMESTAMP,
+        updated_at TIMESTAMP,
+        FOREIGN KEY (profile_id) REFERENCES profiles (id),
+        FOREIGN KEY (community_id) REFERENCES communities (id),
+        FOREIGN KEY (role_id) REFERENCES community_roles (id)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS notifications (
+        id char(36) PRIMARY KEY NOT NULL,
+        type varchar(255),
+        notifiable_type bigint unsigned,
+        notifiable_id bigint unsigned,
+        data json,
+        read_at TIMESTAMP,
+        created_at TIMESTAMP,
+        updated_at TIMESTAMP
+    );
+
+CREATE TABLE
+    IF NOT EXISTS profile_data (
+        id bigint unsigned PRIMARY KEY AUTO_INCREMENT,
+        profile_id bigint unsigned,
+        website_url varchar(255),
+        country varchar(255),
+        city varchar(255),
+        address varchar(255),
+        FOREIGN KEY (profile_id) REFERENCES profiles (id)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS posts (
+        id bigint unsigned PRIMARY KEY AUTO_INCREMENT,
+        author_id bigint unsigned,
+        title varchar(255),
+        body text,
+        uuid62 varchar(255),
+        pageable_type varchar(255),
+        pageable_id bigint unsigned,
+        slug varchar(255),
+        created_at TIMESTAMP,
+        updated_at TIMESTAMP,
+        deleted_at TIMESTAMP,
+        reason_deleted varchar(255),
+        FOREIGN KEY (author_id) REFERENCES profiles (id)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS post_views (
+        id bigint unsigned PRIMARY KEY AUTO_INCREMENT,
+        viewer_id bigint unsigned,
+        post_id bigint unsigned,
+        viewed_count smallint unsigned,
+        viewed_at TIMESTAMP,
+        deleted_at TIMESTAMP,
+        reason_deleted varchar(255),
+        FOREIGN KEY (viewer_id) REFERENCES profiles (id),
+        FOREIGN KEY (post_id) REFERENCES posts (id)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS users_settings (
+        user_id bigint unsigned PRIMARY KEY AUTO_INCREMENT,
+        id bigint unsigned,
+        FOREIGN KEY (user_id) REFERENCES users (id)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS images (
+        id bigint unsigned PRIMARY KEY AUTO_INCREMENT,
+        imageable_type varchar(255),
+        imageable_id bigint unsigned,
+        purpose varchar(255),
+        sha256 char(64),
+        type tinyint unsigned,
+        width smallint unsigned,
+        height smallint unsigned,
+        size bigint unsigned,
+        origin_name varchar(255),
+        mime varchar(255),
+        extension varchar(255),
+        sfw_score decimal(2, 1),
+        created_at TIMESTAMP,
+        updated_at TIMESTAMP,
+        deleted_at TIMESTAMP,
+        reason_deleted varchar(255),
+        FOREIGN KEY (imageable_id) REFERENCES profiles (id)
     );
